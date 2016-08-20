f1f5d9ff43714767ca3ad53ea89f2ea69105f330
My Nguyen <My@mys-macbook-pro.local>
2016-08-20 10:46:45 -0700

    Lesson 2: created Brain class

---
+++ Calculateur/Brain.swift
@@ -0,0 +1,29 @@
+//
+//  Brain.swift
+//  Calculateur
+//
+//  Created by My Nguyen on 8/20/16.
+//  Copyright © 2016 My Nguyen. All rights reserved.
+//
+
+// since this is a model class, so it imports Foundation and not UIKit (which is for UI stuff)
+// this file is created via menu File > New > File. Then choose iOS > Source > Swift File
+import Foundation
+
+class Brain {
+
+    // read-only property (only get is implemented)
+    var result: Double {
+        get {
+            return 0.0
+        }
+    }
+
+    func setOperand(operand: Double) {
+        
+    }
+
+    func perform(symbol: String) {
+        
+    }
+}
--- Calculateur/ViewController.swift
+++ Calculateur/ViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var display: UILabel!
-    var userIsTyping = false
+    @IBOutlet private weak var display: UILabel!
+    private var userIsTyping = false
+    // a computed property; here it's used to facilitate converting between Double and String
+    private var displayValue: Double {
+        get {
+            // need to unwrap the Double because the text string may not be convertible to a Double, e.g. "Hello"
+            return Double(display.text!)!
+        }
+        set {
+            // newValue is a keyword
+            display.text = String(newValue)
+        }
+    }
+    private var brain = Brain()
 
-    @IBAction func touchDigit(sender: UIButton) {
+    @IBAction private func touchDigit(sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsTyping {
             let text = display.text!
@@ -24,11 +36,13 @@ class ViewController: UIViewController {
         userIsTyping = true
     }
 
-    @IBAction func performOperation(sender: UIButton) {
+    @IBAction private func performOperation(sender: UIButton) {
         userIsTyping = false
         if let symbol = sender.currentTitle {
             if symbol == "π" {
-                display.text = String(M_PI)
+                displayValue = M_PI
+            } else if symbol == "√" {
+                displayValue = sqrt(displayValue)
             }
         }
     }
